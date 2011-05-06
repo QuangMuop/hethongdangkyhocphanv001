2c44e2731e4981e8a004f96e398790b388b86f97
locnv.uit <locnv.uit@gmail.com>
2011-05-06 10:51:55 +0000

    test first page

--- DangKyHocPhan/src/java/system/servlets/ViewListClass.java
+++ DangKyHocPhan/src/java/system/servlets/ViewListClass.java
@@ -1,20 +1,15 @@
 package system.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.jms.Session;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import system.access.mapper.clsMapperClass;
-import system.access.mapper.clsMapperLecturer;
 import system.bo.clsBOClass;
 import system.bo.clsBOLecturer;
 import system.bo.clsBOSubject;
@@ -40,19 +35,75 @@ public class ViewListClass extends HttpServlet {
     }
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {                        
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+//        session.removeAttribute("listclass");
+//        session.removeAttribute("listsubject");
+//        session.removeAttribute("listlecturername");
+//        session.removeAttribute("listsubjectnamefull");
+//        session.removeAttribute("listlecturernamefull");
+
         String isSearchEngine = (String) req.getParameter("searchengine");
+
+        SetDataForSearchEngine(req, resp, session);
+
         if(isSearchEngine.equalsIgnoreCase("true")){
-            SetDataWithSearchEngine(req, resp);
+            SetDataWithSearchEngine(req, resp, session);
         }else if(isSearchEngine.equalsIgnoreCase("false")){
-            SetDataWithoutSearchEngine(req, resp);
-           
+            SetDataWithoutSearchEngine(req, resp, session);           
         }
+
+        String path = "./jsps/jspXemDSLop.jsp";
+        //RequestDispatcher requestDispatcher = req.getRequestDispatcher(path);
+        //requestDispatcher.forward(req, resp);
+        resp.sendRedirect(path);
     }
 
-    private void SetDataWithoutSearchEngine(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        HttpSession session = req.getSession();
+    /**
+     *Get search title: lecturer's name and subject's name
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void SetDataForSearchEngine(HttpServletRequest req, HttpServletResponse resp, HttpSession session)
+            throws ServletException, IOException {
+        ArrayList<String> listLectureName = new ArrayList<String>();
+        ArrayList<String> listSubjectName = new ArrayList<String>();
+
+        int i,j,n,m;
+        try {
+            clsBOClass classBo = new clsBOClass();
+            clsBOLecturer lecturerBo = new clsBOLecturer();
+            clsBOSubject subjectBo = new clsBOSubject();
+
+            ArrayList<clsClass> listClass = classBo.GetAllClass();
+            n = listClass.size();
+            for(i = 0; i < n; i++){
+                String subjectId = listClass.get(i).getSubCode();
+                String subjectName = subjectBo.getSubjectNameByCode(subjectId);
+                listSubjectName.add(subjectName);
+
+                String lectureId = listClass.get(i).getLectureCode();
+                String lectureName = lecturerBo.LecturerGetLecturerNameFromId(lectureId);
+                listLectureName.add(lectureName);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(ViewListClass.class.getName()).log(Level.SEVERE, null, ex);
+        }               
+        session.setAttribute("listsubjectnamefull", listSubjectName);
+        session.setAttribute("listlecturernamefull", listLectureName);
+    }
+
+    /**
+     * Get data from datbase, put all into sesstion. Do without search selection
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void SetDataWithoutSearchEngine(HttpServletRequest req, HttpServletResponse resp, HttpSession session)
+            throws ServletException, IOException {
         ArrayList<clsClass> listClass = new ArrayList<clsClass>();
         ArrayList<clsSubject> listSubject = new ArrayList<clsSubject>();
         ArrayList<String> listLecturerName = new ArrayList<String>();
@@ -78,21 +129,28 @@ public class ViewListClass extends HttpServlet {
         } catch (Exception ex) {
             Logger.getLogger(ViewListClass.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-
-        session.setAttribute("a", "toi khong hieu tai sao lainhu vay/...");
+      
         session.setAttribute("listclass", listClass);
         session.setAttribute("listsubject", listSubject);
-        session.setAttribute("listlecturername", listLecturerName);
-
-        String path = "./jsps/jspXemDSLop.jsp";
-        //RequestDispatcher requestDispatcher = req.getRequestDispatcher(path);
-        //requestDispatcher.forward(req, resp);
-        resp.sendRedirect(path);
+        session.setAttribute("listlecturername", listLecturerName);        
     }
 
-    private void SetDataWithSearchEngine(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.println("Co search");
+    /**
+     *Get data from datbase, put all into sesstion. Do without search selection
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void SetDataWithSearchEngine(HttpServletRequest req, HttpServletResponse resp, HttpSession session)
+            throws ServletException, IOException {
+        ArrayList<clsClass> listClass = new ArrayList<clsClass>();
+        ArrayList<clsSubject> listSubject = new ArrayList<clsSubject>();
+        ArrayList<String> listLecturerName = new ArrayList<String>();
+
+        
+        session.setAttribute("listclass", null);
+        session.setAttribute("listsubject", null);
+        session.setAttribute("listlecturername", null);
     }
 }
